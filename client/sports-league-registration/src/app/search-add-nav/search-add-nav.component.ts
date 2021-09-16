5bfb85645b1bc2e0bc891f334be0f02b57bf8b02
Anthony Lopez-Guerra <anthony.lopez.guerra@jbhunt.com>
2021-09-16 08:45:56 -0500

    changed select input to search

--- client/sports-league-registration/src/app/search-add-nav/search-add-nav.component.ts
+++ client/sports-league-registration/src/app/search-add-nav/search-add-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, UrlSegment } from '@angular/router';
 import { UrlService } from '../services/url.service';
 
@@ -12,44 +12,44 @@ export class SearchAddNavComponent implements OnInit {
   workflow: string;
 
   constructor(private urlService: UrlService, private router: Router) { }
+  searchedValue: string = '';
 
   ngOnInit(): void {
     
   }
+  ngAfterContentInit() {
+    console.log(this.searchedValue)
+  }
+
+  search(searchedValue) {
+    console.log(searchedValue.target.value)
+    this.urlService.setSearchedValue(searchedValue.target.value);
+  }
 
   add() {
-console.log(this.router.url);
+    console.log(this.urlService.getSearchedValue());
     this.url = this.urlService.getUrl(this.router.url);
     this.workflow = this.url[1];
     if(this.workflow === 'sports') {
-      console.log("I am adding a sport");
       if(this.url.length === 2){
         this.router.navigateByUrl(`${this.router.url}/addSport`);
       } else if (this.url.length === 3) {
-        console.log("I am getting bigger size 3");
         this.router.navigateByUrl(`${this.router.url}/addTeam`);
       } else if (this.url.length === 5) {
-        console.log("I am the biggest size 5");
         this.router.navigateByUrl(`${this.router.url}/addPlayer`);
       }
-
     } else if (this.workflow === 'teams'){
-      console.log("I am adding a team")
       if(this.url.length === 2){
         this.router.navigateByUrl(`${this.router.url}/addTeam`);
       } else if(this.url.length === 3) {
-        console.log("I am getting bigger size 3 add sport to team");
         this.router.navigateByUrl(`${this.router.url}/addSport`);
       } else if(this.url.length === 5) {
-        console.log("I am the biggest size 5 adding player to team");
         this.router.navigateByUrl(`${this.router.url}/addPlayer`);
       }
     } else if (this.workflow === 'coaches') {
-      console.log("I am adding a coach")
       this.router.navigateByUrl(`${this.router.url}/addCoaches`);
     }
     else {
-      console.log("I am adding player")
       if(this.router.url !== `${this.router.url}/addPlayer`) {
         this.router.navigateByUrl(`${this.router.url}/addPlayer`);
       }
--- client/sports-league-registration/src/app/services/url.service.ts
+++ client/sports-league-registration/src/app/services/url.service.ts
@@ -8,8 +8,18 @@ import { Observable } from 'rxjs';
 export class UrlService {
 
   constructor(private router: ActivatedRoute) { }
+  searchedValue;
   
   getUrl(path: string): string[] {
     return path.split('/');
+  }
+
+  setSearchedValue(value) {
+    this.searchedValue = value;
+    console.log('set searched Value to: ' + this.searchedValue)
+  }
+
+  getSearchedValue() {
+    return this.searchedValue;
   }
 }
